347d559a661737434f2ea9ad91378e746101559b
NTH19 <3310288189@qq.com>
2023-02-21 09:44:06 +0800

    add some msg info

---
+++ AsyncRpc/util/msg_req.h
@@ -0,0 +1,18 @@
+#ifndef TINYRPC_COMM_MSG_REQ_H
+#define TINYRPC_COMM_MSG_REQ_H
+
+#include <string>
+
+namespace AsyncRpc {
+
+class MsgReqUtil {
+ public:
+  static std::string genMsgNumber();
+
+};
+
+
+}
+
+
+#endif
